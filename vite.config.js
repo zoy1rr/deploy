85649c8eaa3e874b94d6fb2766bf307758f78412
Zoyir <zoyir9500@gmail.com>
2025-09-27 09:58:42 +0500

    vite.config.js qo‘shildi va _redirects fayli qo‘shildi

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Netlify va boshqa hostinglarda to‘g‘ri ishlashi uchun
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        // Fayl nomlariga hash qo‘shiladi (keshlash uchun yaxshi)
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
 })
